95c18f5ef243fce55beb68f8b5fa14c0b84690c9
Vikas-aws-2023 <vikas@carcility.com>
2023-06-21 01:32:44 +0530

    update-message

--- roboshop/Components/mongodb.sh
+++ roboshop/Components/mongodb.sh
@@ -34,27 +34,29 @@ echo "updating the configeration file"
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf &>> $LOGFILE
 status $?
 
+echo "Start ${COMPONENT}"
+systemctl daemon.reload mongod &>> $LOGFILE
+systemctl enable mongod &>> $LOGFILE
+systemctl restart mongod &>> $LOGFILE
+status $?
+
 echo "Downoad the ${COMPONENT} schema"
 curl -s -L -o /tmp/mongodb.zip "https://github.com/stans-robot-project/mongodb/archive/main.zip" &>> $LOGFILE
 status $?
 
 echo "Extract the ${COMPONENT} component schema"
 cd /tmp
-unzip mongodb.zip &>> $LOGFILE
+unzip -o mongodb.zip &>> $LOGFILE
 status $?
 
 echo "Inject the schema for ${COMPONENT}"
 cd mongodb-main
-mongo < catalogue.js
-mongo < users.js
+mongo < catalogue.js &>> $LOGFILE
+mongo < users.js     &>> $LOGFILE
 status $?
 
 
-echo "Start ${COMPONENT}"
-systemctl daemon.reload mongod &>> $LOGFILE
-systemctl enable mongod &>> $LOGFILE
-systemctl restart mongod &>> $LOGFILE
-status $?
+
 
 # curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/stans-robot-project/mongodb/main/mongo.repo
 # yum install -y mongodb-org
