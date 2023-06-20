20f96dfd9700a4ef616e46c3e19047da9c7149a5
Vikas-aws-2023 <vikas@carcility.com>
2023-06-21 01:16:55 +0530

    update-message

--- roboshop/Components/mongodb.sh
+++ roboshop/Components/mongodb.sh
@@ -34,6 +34,10 @@ systemctl start mongod
 
 status $?
 
+echo "updating the configeration file"
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
+status $?
+
 # curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/stans-robot-project/mongodb/main/mongo.repo
 # yum install -y mongodb-org
 # systemctl enable mongod
