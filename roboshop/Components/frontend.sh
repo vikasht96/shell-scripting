3200999c5c5fda6d5378d79818efb1d884f154b3
Vikas-aws-2023 <vikas@carcility.com>
2023-06-21 00:21:34 +0530

    update-message

--- roboshop/Components/frontend.sh
+++ roboshop/Components/frontend.sh
@@ -23,5 +23,33 @@ fi
 echo "Downloading the ${COMPONENT} component"
 
 curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
+
+if [ $? -eq 0 ]; then
+    echo "Success"
+else
+    echo "Failure"
+fi
+
+echo "Perfoming cleanup"
+cd /usr/share/nginx/html
+rm -rf * &>> $LOGFILE
+
+if [ $? -eq 0 ]; then
+    echo "Success"
+else
+    echo "Failure"
+fi
+
+
+
+
+
+# cd /usr/share/nginx/html
+# rm -rf *
+# unzip /tmp/frontend.zip
+# mv frontend-main/* .
+# mv static/* .
+# rm -rf frontend-main README.md
+# mv localhost.conf /etc/nginx/default.d/roboshop.conf
 # systemctl enable nginx
 # systemctl start nginx
